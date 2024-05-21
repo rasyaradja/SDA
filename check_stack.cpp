72bc84d6578846323f7f0f4c1eede4b68eaa5446
rasyaradja <110718022+rasyaradja@users.noreply.github.com>
2024-05-21 15:06:05 +0700

    Update check_stack.cpp

--- check_stack.cpp
+++ check_stack.cpp
@@ -34,7 +34,7 @@ int main() {
 
     // Mengisi stack kedua dengan nilai yang sama dengan stack pertama
     stack2.push(1);
-    stack2.push(1);
+    stack2.push(2);
     stack2.push(3);
 
     // Mengecek apakah kedua stack memiliki nilai yang sama
